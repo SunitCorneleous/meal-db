259ec6fb872f9cfee75376e0d40a8d166d674fe2
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-08-29 22:05:10 +0600

    files added

---
+++ js/app.js
@@ -0,0 +1,51 @@
+const loadAllMeals = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?f=b")
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+
+const displayMeals = (meals) => {
+  const mealContainer = document.getElementById("all-foods");
+  mealContainer.innerHTML = "";
+  meals.forEach((meal) => {
+    const card = document.createElement("div");
+    card.classList.add("g-col-4");
+    card.setAttribute("style", "cursor:pointer");
+
+    card.innerHTML = `
+    <div class="col">
+        <div class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">Meal origin: ${meal.strArea}.</p>
+                <p class="card-text">Meal category: ${meal.strCategory}.</p>
+            </div>
+        </div>
+    </div>
+      `;
+
+    mealContainer.appendChild(card);
+  });
+};
+
+const searchMeals = (data) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${data}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+
+  // console.log(url);
+};
+
+document.getElementById("search-btn").addEventListener("click", () => {
+  const inputField = document.getElementById("search-input");
+  const inputValue = inputField.value;
+
+  inputField.value = "";
+
+  searchMeals(inputValue);
+});
+
+loadAllMeals();
